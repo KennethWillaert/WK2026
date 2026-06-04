947cdb5fca9877c61298e98ff9b5b78bff6c7f59
Kenneth Willaert <djduuub@gmail.com>
2026-06-04 10:58:32 +0200

    update

--- test_topscorers.sql
+++
@@ -1,5 +0,0 @@
-INSERT OR REPLACE INTO results (match_id, home_score, away_score) VALUES (
-  'topscorers',
-  '[{"name":"Kylian Mbappé","team":"Frankrijk","goals":6},{"name":"Erling Haaland","team":"Noorwegen","goals":5},{"name":"Romelu Lukaku","team":"België","goals":4},{"name":"Lionel Messi","team":"Argentinië","goals":4},{"name":"Cristiano Ronaldo","team":"Portugal","goals":3},{"name":"Harry Kane","team":"Engeland","goals":3},{"name":"Lautaro Martinez","team":"Argentinië","goals":3},{"name":"Robert Lewandowski","team":"Polen","goals":2},{"name":"Vinicius Jr","team":"Brazilië","goals":2},{"name":"Memphis Depay","team":"Nederland","goals":2}]',
-  0
-);
