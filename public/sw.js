e9be778d55e6f286d7b3258699184d1f21bea386
Kenneth Willaert <djduuub@gmail.com>
2026-06-15 15:03:23 +0200

    fix m26/m50 teamnamen Bosnië + cache bust

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-const CACHE = 'wk2026-v' + '20260615';
+const CACHE = 'wk2026-v' + '20260615b';
 const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
 self.addEventListener('install', e => {
