eb7dd118c0350315200ae4c56036375a03cb3a15
Kenneth Willaert <djduuub@gmail.com>
2026-06-16 22:08:23 +0200

    fix iOS auto-scroll: feature-popup uitstellen + robuuste wait-for-render scroll, cache bust

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-const CACHE = 'wk2026-v' + '20260615d';
+const CACHE = 'wk2026-v' + '20260615e';
 const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
 self.addEventListener('install', e => {
