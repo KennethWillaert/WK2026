4ab536c90bee0e647ebf721b0db2443eb713835a
Kenneth Willaert <djduuub@gmail.com>
2026-06-16 23:03:11 +0200

    fix iOS scroll-reset door OneSignal permissie-prompt

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-const CACHE = 'wk2026-v' + '20260615e';
+const CACHE = 'wk2026-v' + '20260615f';
 const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
 self.addEventListener('install', e => {
