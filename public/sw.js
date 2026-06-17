170bfc886e6794441478e8d36475f369cd7b6bed
Kenneth Willaert <djduuub@gmail.com>
2026-06-17 17:05:16 +0200

    update

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-const CACHE = 'wk2026-v' + '20260617a';
+const CACHE = 'wk2026-v' + '20260617b';
 const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
 self.addEventListener('install', e => {
