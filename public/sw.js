baf93c271df6084be3d410242d429877499b5493
Kenneth Willaert <djduuub@gmail.com>
2026-06-28 11:30:55 +0200

    r32 teamnamen ingevuld + tijden correct

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDKWorker.js');
 
-const CACHE = 'wk2026-v' + '20260628a';
+const CACHE = 'wk2026-v' + '20260628b';
 const STATIC = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
 self.addEventListener('install', e => {
